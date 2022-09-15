f288596573af12985f47fefbbe0c5872c1073b90
Sitman <88289890+ToKyoV2@users.noreply.github.com>
2022-09-15 14:15:43 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+--[[----------------------------------
+Creation Date:	07/05/2021
+]]------------------------------------
+fx_version 'adamant'
+game 'gta5'
+author 'Sitman#2191'
+version '1.0.0'
+versioncheck 'https://raw.githubusercontent.com//main/fxmanifest.lua'
+lua54 'yes'
+
+shared_scripts {
+	'@es_extended/imports.lua', -- Remove if you're using a version before ESX 1.3
+	'config.lua'
+}
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+exports {
+	"Notify",
+	"Loading",
+	"playAnim",
+	"addProp"
+}
